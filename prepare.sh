fe6878b7e1c3727d546aed7280bcc97a9686365b
Taz Jackson <tatjsn@users.noreply.github.com>
2023-08-15 07:42:01 +0100

    Revert

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,3 @@
-if [ -n $(which python) ]; then
-    echo "Missing python alias e.g. alias python=/usr/bin/python3"
-    exit 1
-fi
-
 if [ -n "$VIRTUAL_ENV" ]; then
     echo "You're running a Python virtual environment!"
 else
